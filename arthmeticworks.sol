ba01919ad530fb76dd1ed50752038dcf44fe0d4c
Damilola145 <damilolaolonitola807@gmail.com>
2024-10-11 22:05:22 +0100

    Arithmetic Functions

---
+++ arthmeticworks.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.7;
+
+contract Maths {
+         function add(uint x,uint y) external pure returns(uint) {
+            return (x+y);
+         }
+
+
+  function sub(uint x,uint y) external pure returns(uint) {
+            return (x-y);
+         }
+
+
+  function multiply(uint x,uint y) external pure returns(uint) {
+            return (x*y);
+         }
+
+
+
+  function divide(uint x,uint y) external pure returns(uint) {
+            return (x/y);
+         }
+
+}
